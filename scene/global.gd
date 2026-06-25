63156ff9bfa5a692e06e6ab23de33ba6c9144cd8
Fernanda Oshiro <fernanda.sayuri@outlook.com>
2026-06-24 21:26:00 -0300

    aaa

--- scene/global.gd
+++ scene/global.gd
@@ -1,3 +1,48 @@
 extends Node
 
 var score:int = 999
+
+var recognition # O objeto de reconhecimento de voz no JS
+var text_received
+signal texto_reconhecido(texto)
+
+func _ready():
+	# Verifica se estamos rodando no navegador
+	if OS.has_feature("web"):
+		setup_speech_recognition()
+
+func setup_speech_recognition():
+	# Código JavaScript em formato de String
+	var js_code = """
+        window.recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+        window.recognition.lang = 'pt-BR';
+        window.recognition.interimResults = false;
+
+        window.recognition.onresult = function(event) {
+            var text = event.results[0][0].transcript;
+            // Envia o texto de volta para o Godot
+            window.godot_speech_callback(text);
+        };
+    """
+	JavaScriptBridge.eval(js_code)
+	
+	# Cria uma função que o JavaScript pode chamar para devolver o texto
+	var callback = JavaScriptBridge.create_callback(_on_speech_received)
+	JavaScriptBridge.get_interface("window").godot_speech_callback = callback
+
+func start_listening():
+	if OS.has_feature("web"):
+		JavaScriptBridge.eval("window.recognition.start();")
+		print("Ouvindo...")
+		
+
+func stop_listening():
+	if OS.has_feature("web"):
+		JavaScriptBridge.eval("window.recognition.stop();")
+		print("Parou de ouvir.")
+
+# Esta função é chamada quando o navegador entende a voz
+func _on_speech_received(args):
+	text_received = args[0]
+	print("O navegador ouviu: ", text_received)
+	texto_reconhecido.emit(text_received)
--- scene/title.gd
+++ scene/title.gd
@@ -2,6 +2,8 @@ extends Control
 
 func _ready() -> void:
 	$Score.text = "High Score: " + str(Global.score)
+	var speech_node = get_node("/root/Global")
+	speech_node.texto_reconhecido.connect(atualizar_texto)
 
 func _process(_delta: float) -> void:
 	if(Input.is_action_just_pressed("space")):
@@ -9,3 +11,16 @@ func _process(_delta: float) -> void:
 
 func change_scene():
 	get_tree().change_scene_to_file("res://scene/game.tscn")
+
+
+func _on_menu_button_pressed() -> void:
+	Global.start_listening()
+	pass # Replace with function body.
+
+
+func _on_menu_button_2_pressed() -> void:
+	Global.stop_listening()
+	pass # Replace with function body.
+
+func atualizar_texto(text):
+	$Label2.text = text
